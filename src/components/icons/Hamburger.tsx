a5b3fbc2955b44930b917ef737fbf69261c1071d
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-10-24 10:03:33 +0330

    style(Hamburger): correct height and width

--- src/components/icons/Hamburger.tsx
+++ src/components/icons/Hamburger.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Hamburger() {
-  return <Image src="/hamburger.svg" width={24} height={24} alt="Hamburger Menu" />;
+  return (
+    <div className="h-6 w-6">
+      <Image src="/hamburger.svg" width={24} height={24} alt="Hamburger Menu" />
+    </div>
+  );
 }
