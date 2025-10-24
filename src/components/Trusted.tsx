5bcea6287dfc587b8016d27858ca981b718a8d01
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-10-24 09:58:15 +0330

    feat(trusted): develop trusted section

---
+++ src/components/Trusted.tsx
@@ -0,0 +1,31 @@
+import { trans } from "@/lib/utils";
+import Image from "next/image";
+
+const images = ["air.webp", "esb.webp", "space.webp"];
+
+export function Trusted() {
+  return (
+    <div>
+      <div className="inline-flex items-center gap-3 px-4 py-2 rounded-xl bg-yellow-700  ">
+        {/* Overlapping avatars */}
+        <ul className="flex -space-x-2">
+          {images.map((url: string, key) => (
+            <li className="w-6 h-6 rounded-full bg-white overflow-hidden" key={key}>
+              <Image
+                src={`/${url}`}
+                alt={url}
+                className="w-full h-full object-cover"
+                width={24}
+                height={24}
+                loading="lazy"
+              />
+            </li>
+          ))}
+        </ul>
+
+        {/* Text content */}
+        <p className="text-sm font-medium">{trans("trustedBy")}</p>
+      </div>
+    </div>
+  );
+}
