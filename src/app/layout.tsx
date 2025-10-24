beec42d4e55f3b9148334d2da7ac3a286896c330
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-10-24 09:59:15 +0330

    style(fonts): add used font in figma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
 import MobileNavbar from "@/components/MobilNav";
 import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
 });
 
@@ -68,15 +63,14 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          geistSans.variable,
-          geistMono.variable,
+          dmSans.variable,
           "antialiased",
           "h-screen bg-linear-to-r from-[rgba(253,235,179,0.3)] to-[#E1EBE8]"
         )}
       >
         <Nav />
         <MobileNavbar />
-        <main className="container">{children}</main>;
+        <main className="container m-auto">{children}</main>
       </body>
     </html>
   );
